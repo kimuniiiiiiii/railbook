4cf156918dfa0ab2c4d792de37508ee43ae69147
Yuya Kimura <kimukimunii@gmail.com>
2014-10-29 14:39:03 +0900

    Initialize repository

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,15 @@
+#coding:utf-8
+
+class HelloController < ApplicationController
+	def index
+		render :text => 'こんにちは、世界！'
+	end
+
+	def view
+		@msg = 'こんにちは、世界！'
+	end
+
+	def list
+		@books = Book.all
+	end
+end
