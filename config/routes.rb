3703d8acc3c97588b023cabcfe822800adc07268
Yuya Kimura <kimukimunii@gmail.com>
2014-10-31 15:32:32 +0900

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   resources :books
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027063324) do
+ActiveRecord::Schema.define(version: 20141031055000) do
 
   create_table "books", force: true do |t|
     t.string   "isbn"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 20141027063324) do
     t.string   "publish"
     t.date     "published"
     t.boolean  "cd"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
